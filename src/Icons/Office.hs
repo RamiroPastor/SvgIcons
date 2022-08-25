8b209a0cda71bee960c2861d6fdba8dd4b291600
Ramiro <rammir659@gmail.com>
2022-08-25 18:28:09 +0200

    new pencil icon

--- src/Icons/Office.hs
+++ src/Icons/Office.hs
@@ -15,6 +15,7 @@ import Base
 svgOffice :: [ (String , S.Svg) ]
 svgOffice =
   [ (,) "envelope" envelope
+  , (,) "pencil"   pencil
   ]
 
 
@@ -46,4 +47,41 @@ envelope =
       l   ( kx)      (-ky + s)
       l   ( mx)      ( my + s)
       S.z
+      
+
+pencil :: Svg
+pencil =
+    S.path
+      ! d pencilPath
+      ! strokeLinejoin "round"
+  where
+    w  = 0.6   -- width of the pencil
+    x1 = -w/2
+    x2 =  w/2
+    y1 = -0.9
+    y2 = -0.76
+    y3 = -0.6
+    y4 =  0.4
+    y5 =  y6 - 0.15
+    y6 =  0.94
+    pencilPath = mkPath $ do
+      m   0   y5
+      l   0   y6
+      l   x1  y4
+      l   x2  y4
+      l   0   y6
+      m   x1  y4
+      l   x1  y3
+      l   x2  y3
+      l   x2  y4
+      m   0   y4
+      l   0   y3
+      m   x1  y3
+      l   x1  y2
+      l   x2  y2
+      l   x2  y3
+      m   x1  y2
+      l   x1  y1
+      aa  (w/2) 0.03 0 True True x2 y1
+      l   x2  y2
       
