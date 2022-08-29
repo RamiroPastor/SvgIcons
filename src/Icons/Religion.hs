566f22355c9bb8414cc927a20fc9dbef57336f17
Ramiro <rammir659@gmail.com>
2022-08-29 02:06:50 +0200

    xp icon finished

--- src/Icons/Religion.hs
+++ src/Icons/Religion.hs
@@ -26,16 +26,27 @@ xp =
   S.g $ do
     S.path
       ! A.d rho
+      ! A.fillRule "evenodd"
     S.path 
       ! A.d chi
       ! A.transform (rotateAround 45 0 0)
-    frame (-1) (-1) 2 2
+    S.path
+      ! A.d alpha
+      ! A.fill "none"
+      ! A.strokeLinecap  "round"
+      ! A.strokeLinejoin "round"
+    S.path
+      ! A.d omega
+      ! A.fill "none"
+      ! A.strokeLinecap  "round"
+      ! A.strokeLinejoin "round"
   where
     w = 0.07
     k1 = 0.5
     k2 = 0.96
-    r1 = 0.17
-    r2 = r1 - w
+    k3 = 0.17
+    r1 = 0.2
+    a  = 0.3
     chi = mkPath $ do
       m   (   -w)  (   -w)
       l   (   -w)  (-k1+w)
@@ -66,5 +77,22 @@ xp =
       q   (   -w)  ( k2  )  ( -2*w)  ( k2)
       l   (  2*w)  ( k2  )
       q   (    w)  ( k2  )  (    w)  ( k2-w)
-      l   (    w)  ( k2-2*r1)
+      l   (    w)  (-k2+2*r1)
+      l   ( k3+w)  (-k2+2*r1)
+      aa   r1   r1   0   True  False ( k3+w)  (-k2)
       S.z
+      m   (    w)  (-k2+2*r1 - w)
+      c   (2*k3+w)  (-k2+2*r1)  (2*k3+w)  (-k2)  ( w) (-k2 + w)
+      S.z
+    alpha = mkPath $ do
+      m  (-0.7-a/2)  ( a/2)
+      l  (-0.7    )  (-a/2)
+      l  (-0.7+a/2)  ( a/2)
+      m  (-0.7-a/4)  ( 0  )
+      l  (-0.7    )  ( a/4)
+      l  (-0.7+a/4)  ( 0  )
+    omega = mkPath $ do
+      m  ( 0.7-a/2)  ( a/2)
+      l  ( 0.7-a/5)  ( a/2)
+      aa (a/2)  (a/2)  0  True  True  (0.7+a/5) (a/2)
+      l  ( 0.7+a/2)  ( a/2)
