e6b61f390fb8688b659e111234147fd88481a616
Ramiro <rammir659@gmail.com>
2022-09-16 02:05:56 +0200

    standard height and width changed to 100px

--- src/Core/Style.hs
+++ src/Core/Style.hs
@@ -25,8 +25,8 @@ stdDims :: S.Svg -> S.Svg
 stdDims content =
   S.svg content
     ! A.viewbox "-1 -1 2 2"
-    ! A.height  "300px"
-    ! A.width   "300px"
+    ! A.height  "100px"
+    ! A.width   "100px"
 
 
 
