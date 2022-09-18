a6f06662db3374f9d7143d6c366af0989bb981b9
Ramiro <rammir659@gmail.com>
2022-09-18 21:41:10 +0200

    Finished documentating icons and updated version number

--- src/Icons.hs
+++ src/Icons.hs
@@ -34,6 +34,8 @@ For example, if you want to use the `warning` icon from `Icons.Computer`:
 >import           Text.Blaze.Svg11 as S
 >import           Text.Blaze.Svg11.Attributes as A
 >
+>import Icons.Computer (warning)
+>
 >iconExample1 :: Svg
 >iconExample1 =
 >  S.svg
@@ -47,7 +49,93 @@ For example, if you want to use the `warning` icon from `Icons.Computer`:
 
 You can render the result into a @.svg@ file and you will get:
 
+![icon example 1](https://raw.githubusercontent.com/RamiroPastor/SvgIcons/main/svg/examples/iconExample1.svg)
 
+Note that the functions in the previous example come from the 
+[blaze-svg](https://hackage.haskell.org/package/blaze-svg)
+package, so you will need to add it to your dependencies.
+
+There are some functions defined in module `Core.Style` that can make
+this process easier, in particular `stdDims`. Remember that you can avoid
+directly coding @fill@, @stroke@, @stroke-width@, @width@ or @height@ (and more)
+if you plan to use the svg inline (directly in your HTML code) and set those 
+properties with CSS (but it won't work for @\<img\>@ tags)
+
+Example with Core.Style:
+
+>import Core.Style
+>import Icons.Computer (accept)
+>
+>iconExample2 :: Svg
+>iconExample2 = 
+>  stdDims $ fillStyle accept
+
+Resulting svg:
+
+![icon example 2](https://raw.githubusercontent.com/RamiroPastor/SvgIcons/main/svg/examples/iconExample2.svg)
+
+All icon images in the documentation have been generated using
+the three style functions found in `Core.Style`, to expose how each icon looks
+with only stroke and no fill, only fill and no stroke, and both fill and stroke ("full" style).
+Most of them work with all 3 styles, but some are of no use in one of the three cases.
+
+** Composing Icons
+
+As said before, all icons aim to be composable (mix icons in the same svg element).
+Again, this is achieved by not wrapping them in the @\<svg\>@ tag in their definitions,
+and leaving the outmost tag of every icon (usually a @path@ or a @g@ element) free of 
+any transform attribute (and free of almost every other attribute too). Even those icons that
+have some transformation inside their code already, are wrapped with a clean @g@ tag to allow 
+for further transformations.
+
+Example:
+
+(remember that svg transformations are correctly applied from right to left, 
+as it should be because it's just function composition)
+
+>{-# LANGUAGE     OverloadedStrings       #-}
+>
+>import           Text.Blaze.Svg11 ((!))
+>import           Text.Blaze.Svg11 as S
+>import           Text.Blaze.Svg11.Attributes as A
+>
+>import Core.Geometry (starPolygonFirstSpecies)
+>import Icons.Cosmos (moonHalf, sun)
+>
+>
+>iconExample3 :: Svg
+>iconExample3 =
+>  S.svg
+>    ! A.viewbox "-3 -1 6 2"
+>    ! A.width  "300px"
+>    ! A.height "100px"
+>    $ S.g 
+>      ! A.strokeWidth "0.03"
+>      $ do
+>        moonHalf
+>          ! A.fill   "silver"
+>          ! A.stroke "gray"
+>          ! A.transform (translate (-2) 0 <> rotateAround (-30) 0 0 <> S.scale 0.7 0.7)
+>        sun 11
+>          ! A.fill   "yellow"
+>          ! A.stroke "orange"
+>          ! A.transform (translate ( 2) 0)
+>        starPolygonFirstSpecies 5 0.5 (0,0)
+>          ! A.fill   "sandybrown"
+>          ! A.stroke "crimson"
+
+Resulting svg composition:
+
+![icon example 2](https://raw.githubusercontent.com/RamiroPastor/SvgIcons/main/svg/examples/iconExample3.svg)
+
+** Multiple Fill Colors
+
+Currently, there is no way to fill different parts of one icon with different colors
+(or more exotic, animate specific parts of some icon).
+I am reluctant to parametrize colors as arguments for every icon, but this could be achieved
+by giving a unique id or class to each component of every icon. This is not on my priority list
+at this moment, but do not hesitate to ask for it if you need it. Or even better, do it yourself and
+ask for a branch merge on github.
 
 -}
 module Icons 
